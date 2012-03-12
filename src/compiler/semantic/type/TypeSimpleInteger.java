1771acf4adb7c69e5d7c1941a9805d1cc9fe7d50
amedey <amedey@localhost>
2012-03-12 19:07:10 +0100

    Implementada declaraci?n de constantes enteras. Creadas clases SymbolIntegerConstant y TypeSimpleInteger para facilitar el an?lisis sem?ntico.

---
+++ src/compiler/semantic/type/TypeSimpleInteger.java
@@ -0,0 +1,11 @@
+package compiler.semantic.type;
+
+import es.uned.lsi.compiler.semantic.ScopeIF;
+
+public class TypeSimpleInteger extends TypeSimple {
+
+	public TypeSimpleInteger(ScopeIF scope) {
+		super(scope, "INTEGER");
+	}
+
+}
