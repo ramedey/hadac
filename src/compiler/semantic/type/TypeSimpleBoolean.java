7ccbc0af3363ad688d64e4afbc6da7c58b764684
amedey <amedey@localhost>
2012-03-15 10:25:53 +0100

    Creada clase TypeSimpleBoolean.
    Modificada producci?n tipo_simple para crear la clase TypeSimpleBoolean si el tipo es booleano.

---
+++ src/compiler/semantic/type/TypeSimpleBoolean.java
@@ -0,0 +1,10 @@
+package compiler.semantic.type;
+
+import es.uned.lsi.compiler.semantic.ScopeIF;
+
+public class TypeSimpleBoolean extends TypeSimple {
+
+	public TypeSimpleBoolean(ScopeIF scope) {
+		super(scope, "BOOLEAN");
+	}
+}
