831b49bc625ae9b786ed95c82db7227a1431d566
amedey <amedey@localhost>
2012-03-30 13:15:01 +0200

    Realizada la suma de expresiones

--- src/compiler/syntax/nonTerminal/ExpresionAritmetica.java
+++ src/compiler/syntax/nonTerminal/ExpresionAritmetica.java
@@ -2,7 +2,29 @@ package compiler.syntax.nonTerminal;
 
 public class ExpresionAritmetica extends Expresion {
 
+	// valor entero del contexto de la expresión
+	private int value;
+	
+	/**
+	 * @return the value
+	 */
+	public int getValue() {
+		return value;
+	}
+
+	/**
+	 * @param value the value to set
+	 */
+	public void setValue(int value) {
+		this.value = value;
+	}
+
 	public ExpresionAritmetica(){
 		super();
+	}
+	
+	public ExpresionAritmetica(int value){
+		super();
+		this.value = value;
 	}
 }
