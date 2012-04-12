4e3753166e164fc0a620c61a911d72664b8705b6
amedey <amedey@localhost>
2012-04-12 18:27:21 +0200

    Comienzo a implementar acciones semanticas para el acceso a campos de registro.

--- src/compiler/semantic/util/Util.java
+++ src/compiler/semantic/util/Util.java
@@ -2,11 +2,13 @@ package compiler.semantic.util;
 
 import java.util.List;
 
+import compiler.CompilerContext;
 import compiler.lexical.Token;
 import compiler.semantic.symbol.SymbolBooleanConstant;
 import compiler.semantic.symbol.SymbolVariable;
 
 import es.uned.lsi.compiler.semantic.ScopeIF;
+import es.uned.lsi.compiler.semantic.symbol.SymbolIF;
 import es.uned.lsi.compiler.semantic.symbol.SymbolTableIF;
 import es.uned.lsi.compiler.semantic.type.TypeIF;
 import es.uned.lsi.compiler.semantic.type.TypeTableIF;
@@ -69,5 +71,26 @@ public class Util {
 			return false;
 		}
 		throw new Exception("La cadena de entrada no se puede convertir: " + cadena);
+	}
+	
+	/**
+	 * Comprueba si se ha registrado la tabla de símbolos del ámbito
+	 * pasado como parámetro el símbolo pasado como parámetro.
+	 * @param textoSimbolo Cadena de caracteres con el nombre del símbolo
+	 * @param scope Ámbito donde se comprueba la existencia del símbolo.
+	 */
+	public static void comprobarExisteSimboloEnAmbito(String textoSimbolo, ScopeIF scope)
+	{
+		if(!scope.getSymbolTable().containsSymbol(textoSimbolo))
+		{
+			CompilerContext.getSemanticErrorManager().semanticFatalError("El símbolo " + textoSimbolo + " no existe en el ámbito actual");
+		}
+		
+//		SymbolIF simbolo = scope.getSymbolTable().getSymbol(textoSimbolo);
+//		
+//		if(!scope.getTypeTable().containsType(simbolo.getType()))
+//		{
+//			CompilerContext.getSemanticErrorManager().semanticFatalError("El símbolo " + textoSimbolo + " no existe en el ámbito actual");
+//		}
 	}
 }
