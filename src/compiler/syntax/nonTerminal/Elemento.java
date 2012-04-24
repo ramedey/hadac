8b7153e96af9928c69332caa14ee4041e009d9ac
amedey <amedey@localhost>
2012-04-24 10:21:43 +0200

    Solucionada asignaci?n de constante a variable o entre variables.

--- src/compiler/syntax/nonTerminal/Elemento.java
+++ src/compiler/syntax/nonTerminal/Elemento.java
@@ -1,7 +1,11 @@
 package compiler.syntax.nonTerminal;
 
+import compiler.CompilerContext;
+import compiler.semantic.symbol.SymbolBooleanConstant;
+import compiler.semantic.symbol.SymbolIntegerConstant;
 import compiler.semantic.symbol.SymbolVariable;
 
+import es.uned.lsi.compiler.semantic.symbol.SymbolIF;
 import es.uned.lsi.compiler.semantic.type.TypeIF;
 
 public class Elemento extends Expresion {
@@ -32,6 +36,21 @@ public class Elemento extends Expresion {
 	{
 		super(variable.getType());
 		resultado = variable.getValor();
+	}
+	
+	public Elemento(SymbolIF simbolo)
+	{
+		super(simbolo.getType());
+		if(simbolo instanceof SymbolVariable)
+        {
+			resultado = ((SymbolVariable)simbolo).getValor();
+        }else if(simbolo instanceof SymbolBooleanConstant){
+        	resultado = ((SymbolBooleanConstant)simbolo).getValue();
+        }else if(simbolo instanceof SymbolIntegerConstant){
+        	resultado = ((SymbolIntegerConstant)simbolo).getValue();
+        }else{
+        	CompilerContext.getSemanticErrorManager().semanticFatalError("El identificador debe ser una variable o constante");
+        }
 	}
 	
 	@Override
