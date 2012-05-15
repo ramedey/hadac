beb3e3208cc58af55ef0f92652a75b4fbfec31ed
amedey <amedey@localhost>
2012-05-15 20:27:56 +0200

    Generado codigo final para TEst02. EL test lo pasa correctamente.

--- src/compiler/code/translator/TranslatorFinal.java
+++ src/compiler/code/translator/TranslatorFinal.java
@@ -6,8 +6,10 @@ public class TranslatorFinal extends TranslatorBase {
 
 	@Override
 	public void translate(QuadrupleIF q) {
+		getTranslation().append("HALT\n");
+		getTranslation().append("ORG 32768\n");
 		getTranslation().append(LabelManager.getLabels());
-		getTranslation().append("HALT");
+		getTranslation().append("END");
 		
 	}
 
--- src/compiler/code/translator/TranslatorIO.java
+++ src/compiler/code/translator/TranslatorIO.java
@@ -10,10 +10,10 @@ public class TranslatorIO extends TranslatorBase {
 	@Override
 	public void translate(QuadrupleIF q) {
 		
-		getTranslation().append("WRCHAR ");
+		getTranslation().append("WRSTR ");
 		String label = LabelManager.getLabelText();
-		getTranslation().append("/" + label);
-		LabelManager.addLabel(label, "DATA " + q.getFirstOperand());
+		getTranslation().append("/" + label + "\nWRCHAR #10\nWRCHAR #13");
+		LabelManager.addLabel(label, "DATA " + q.getFirstOperand().toString());
 		
 	}
 
